2a5e87e2d847fd0a51c12f08198e827f4a8c949c
codecraft-sanju <sanjusirvich18@gmail.com>
2025-01-14 20:57:06 +0530

    store files added

--- 3-myntra-react-clone/src/main.jsx
+++ 3-myntra-react-clone/src/main.jsx
@@ -1,44 +1,31 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-
-// //create a router
-// const router = createBrowserRouter[]
-
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-
-
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'; 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './routes/App.jsx';
 import './index.css';
-import App from './App.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Bag from './routes/Bag.jsx';
+import Home from './routes/Home.jsx';
+import { Provider } from 'react-redux';
+import myntraStore from './store/index.js';
 
-// Create a router
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />, // Define the root route
-    // Add child routes or more routes as needed
+    element: <App />,
+    children: [
+      { path: '/', element: <Home /> },
+      {
+        path: '/bag',
+        element: <Bag />,
+      },
+    ],
   },
-  // Example of additional routes:
-  // {
-  //   path: '/about',
-  //   element: <About />, // Add an "About" component
-  // },
 ]);
-
-// Render the app with RouterProvider
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} /> {/* Provide the router to your app */}
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={myntraStore}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>,
 );
-
